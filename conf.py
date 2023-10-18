57de56a0711cc92dcfc80f6e31fa4444b948aa09
Will Won <william.won@gatech.edu>
2023-10-18 00:00:21 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,17 +1,20 @@
+# Project Setup
 project = 'RTD Example'
 copyright = '2023, Will Won'
 author = 'Will Won'
 
+# Version of documentation
+version = '1.0'
+release = version
+
+# Sphinx Setup: Extension and theme
 extensions = ['myst_parser',
-              'sphinx_rtd_theme'
-             ]
-
+              'sphinx_rtd_theme',
+              'sphinx_multiversion']
 myst_enable_extensions = ['colon_fence']
-
-templates_path = ['_templates']
-
-exclude_patterns = ['_build', '.DS_Store']
-
 html_theme = 'sphinx_rtd_theme'
 
-html_static_path = ['_static', 'README.md']
+# Sphinx Setup: Paths
+templates_path = ['_templates']
+exclude_patterns = ['_build', '.DS_Store', 'README.md']
+html_static_path = ['_static']
